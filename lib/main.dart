823846ccee64973a5b4b521da7d7c8c428a049a3
Md. Nazim Uddin <37615644+nazimfeni@users.noreply.github.com>
2024-02-14 19:39:06 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:crafty_bay/app.dart';
 import 'package:flutter/material.dart';
 
-/// Layer first - Layer (Data, Ui, Domain)
-/// Feature first - Login (Layer), Cart(Layer)
-///
-/// Good code, Bad code
-/// Bad Code - Code Smell
-/// Good code - Readibility, reusability, extend
-/// Refactoring - Process - Bad - clean - Good
-/// KISS - Keep It Simple, Stupid
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
